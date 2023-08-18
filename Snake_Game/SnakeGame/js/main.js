03d118ad0e63127a274e5685c04d0c8180c4eeb8
tesfahunfola <122350783+tesfahunfola@users.noreply.github.com>
2023-08-18 21:32:05 +0300

    Update main.js

--- Snake_Game/SnakeGame/js/main.js
+++ Snake_Game/SnakeGame/js/main.js
@@ -2,28 +2,18 @@
 user_api = AIzaSyAX9FVOSR_4M_qn3PeozAGGNw3Rn_dkDbM
 
 async function SnakeGameProject() {
-	await fetch("https://covid-193.p.rapidapi.com/statistics", {
+	await fetch("https://snakegame.p.rapidapi.com/statistic", {
 		method: "GET",
 		headers: {
 			"X-RapidAPI-Key": "2b1a2679a3msh2f05c5afba35dc2p1ed8c4jsnd91091012b68",
-			"X-RapidAPI-Host": "covid-193.p.rapidapi.com",
+			"X-RapidAPI-Host": "snakegame.p.rapidapi.com",
 		},
 	})
 		.then((response) => response.json())
 		.then((response) => {
-			console.log("COVID 19 API object:")
+			console.log("SNAKE GAME API object:")
 			console.log(response)
 			console.log("\n")
-
-			// This code adds all countries to select element
-			response.response.forEach((c) => {
-				const option = document.createElement("option")
-				option.innerHTML = c.country
-				document.getElementById("countries").appendChild(option)
-			})
-
-			// This code saves covid data to global variable
-			covid19data = response.response
 		})
 		.catch((err) => {
 			console.log(err)
